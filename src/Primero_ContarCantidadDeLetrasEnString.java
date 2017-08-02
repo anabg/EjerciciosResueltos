6d6e701f3b1a7d5f7720d0b491149239d403b1c7
belen <anitagrimaut@gmail.com>
2017-08-02 12:23:37 -0700

    with changes

--- src/Primero_ContarCantidadDeLetrasEnString.java
+++ src/Primero_ContarCantidadDeLetrasEnString.java
@@ -1,8 +1,25 @@
 
+
+/*Compress String with character and its count. 
+Example: "aaabbba" -> compress -> "a3b3a1”*/ 
+
 public class Primero_ContarCantidadDeLetrasEnString {
 	public static void main(String[] args) {
 
 		String word = "aaaabccdddef";
+		System.out.println(" Original Word: " + word + " compressed word " + compressString(word));
+		
+		String word2 = "abbcccddddeeeeefffff";
+		System.out.println(" Original Word: " + word2 + " compressed word " + compressString(word2));
+		
+		String word3 = "ana";
+		System.out.println(" Original Word: " + word3 + " compressed word " + compressString(word3));
+		
+
+	}
+
+	private static String compressString(String word) {
+		
 
 		char[] a = word.toCharArray();
 		StringBuilder endWord = new StringBuilder();
@@ -39,6 +56,6 @@ public class Primero_ContarCantidadDeLetrasEnString {
 			}
 		}
 		System.out.println(endWord);
-
+		return endWord.toString();
 	}
 }
