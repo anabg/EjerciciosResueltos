3f3aad2a9fd0679d6bb0e4ade154e04a97e438dc
anabg <anitagrimaut@gmail.com>
2017-08-03 00:47:07 -0300

    add new examples without solution

---
+++ src/Cuarto_WriteAPattern.java
@@ -0,0 +1,26 @@
+
+public class Cuarto_WriteAPattern {
+
+	
+	/*
+	 * 10. Print following structure in Java? This program is a good exercise
+	 * for mastering loops e.g. for loop and while loop in Java. This also
+	 * teaches you How to use break and continue statement with loops in Java.
+	 * By the way you can print any character and use System.out.print() and
+	 * System.out.println())
+	 *
+	 ***
+	 *****
+	 ***
+	 *
+	 
+	 * 
+	 * 
+	 * Read more:
+	 * http://www.java67.com/2013/01/10-programming-questions-and-exercises.html
+	 * #ixzz4oez3Tn3a
+	 */
+	
+	
+	
+}
---
+++ src/Tercero_ReverseAWord.java
@@ -0,0 +1,17 @@
+
+public class Tercero_ReverseAWord {
+
+	
+	/*Write a program in Java to reverse any String without using StringBuffer?
+			This is another classical Java programming question. You can reverse String in various way in Java but two programming technique is used to do e.g. Iteration and Recursion. Try solving this problem using Iteration first by using Java’s arithmetic operator and than look to implement a recursive solution. Here is one way to reverse String in Java without using StringBuffer.
+
+
+			Read more: http://www.java67.com/2013/01/10-programming-questions-and-exercises.html#ixzz4oeydxq00*/
+	
+	
+	
+	public static void main(String args[]){
+		
+		String word = "casa";
+	}
+}
