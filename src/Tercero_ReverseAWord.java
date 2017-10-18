987450a1c28106a68fb3a4fcffcac898d8bcaf91
belen <anitagrimaut@gmail.com>
2017-10-18 11:06:07 -0700

    add new examples resolved

--- src/Tercero_ReverseAWord.java
+++ src/Tercero_ReverseAWord.java
@@ -1,29 +1,35 @@
 
 public class Tercero_ReverseAWord {
 
-	
-	/*Write a program in Java to reverse any String without using StringBuffer?
-			This is another classical Java programming question. You can reverse String in various way in Java but two programming technique is used to do e.g. Iteration and Recursion. Try solving this problem using Iteration first by using Java’s arithmetic operator and than look to implement a recursive solution. Here is one way to reverse String in Java without using StringBuffer.
+	/*
+	 * Write a program in Java to reverse any String without using StringBuffer?
+	 * This is another classical Java programming question. You can reverse String
+	 * in various way in Java but two programming technique is used to do e.g.
+	 * Iteration and Recursion. Try solving this problem using Iteration first by
+	 * using Java’s arithmetic operator and than look to implement a recursive
+	 * solution. Here is one way to reverse String in Java without using
+	 * StringBuffer.
+	 * 
+	 * 
+	 * Read more:
+	 * http://www.java67.com/2013/01/10-programming-questions-and-exercises.html#
+	 * ixzz4oeydxq00
+	 */
 
+	public static void main(String args[]) {
 
-			Read more: http://www.java67.com/2013/01/10-programming-questions-and-exercises.html#ixzz4oeydxq00*/
-	
-	
-	
-	public static void main(String args[]){
-		
 		String word1 = "casa";
 		String finalWord1 = reverseWord(word1);
-		
+
 		String word2 = "perro";
 		String finalWord2 = reverseWord(word2);
-		
+
 		String word3 = "avion";
 		String finalWord3 = reverseWord(word3);
-		
+
 		String word4 = "mesa";
 		String finalWord4 = reverseWord(word4);
-		
+
 		System.out.println(word1 + " reverse: " + finalWord1);
 		System.out.println(word2 + " reverse: " + finalWord2);
 		System.out.println(word3 + " reverse: " + finalWord3);
@@ -32,12 +38,11 @@ public class Tercero_ReverseAWord {
 
 	private static String reverseWord(String word) {
 		String finalWord = "";
-		
-		for(int i = word.length()-1; i>=0 ; i--) {
-			
+
+		for (int i = word.length() - 1; i >= 0; i--) {
+
 			finalWord = finalWord + word.charAt(i);
-			
-			
+
 		}
 		return finalWord;
 	}
---
+++ src/hakerrank/algorithms/warmup/MiniMaxSum.java
@@ -0,0 +1,48 @@
+package hakerrank.algorithms.warmup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+import sun.security.provider.certpath.CollectionCertStore;
+
+public class MiniMaxSum {
+	public static void calculateMaxAndMin(int[] arr){
+        
+        //int[] sum = new int[4];
+        List<Long> sum = new ArrayList();
+        
+        
+        for(int i = 0; i < arr.length ; i++){
+        	int s = 0;
+            for(int j = 0; j < arr.length ; j++){
+                if(i!=j){
+                    s = s + arr[j];
+                }
+                 
+            }
+           sum.add(new Long (s));
+        }
+        
+        java.util.Collections.sort(sum);
+        for (Long object : sum) {
+			System.out.println(object);
+		}
+        
+        System.out.println(sum.get(0) + " " + sum.get(sum.size()-1));
+            
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int arr_i=0; arr_i < 5; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        
+        MiniMaxSum.calculateMaxAndMin(arr);
+    }
+}
+
