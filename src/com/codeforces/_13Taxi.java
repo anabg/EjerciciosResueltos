175f132f4baeb33ff884baf2e8e70a70950021ee
Grimaut, Ana <Ana_Grimaut@McAfee.com>
2019-02-05 15:46:07 -0300

    add resolutions

--- src/com/codeforces/_13Taxi.java
+++ src/com/codeforces/_13Taxi.java
@@ -1,26 +1,26 @@
 package com.codeforces;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Taxi
- *
+ * <p>
  * outputstandard output
- *
+ * <p>
  * After the lessons n groups of schoolchildren went outside and decided to visit Polycarpus to celebrate his birthday.
  * We know that the i-th group consists of si friends (1 ≤ si ≤ 4), and they want to go to Polycarpus together.
  * They decided to get there by taxi. Each car can carry at most four passengers. What minimum number of cars will the
  * children need if all members of each group should ride in the same taxi (but one taxi can take more than one group)?
- *
+ * <p>
  * Input
  * The first line contains integer n (1 ≤ n ≤ 105) — the number of groups of schoolchildren.
  * The second line contains a sequence of integers s1, s2, ..., sn (1 ≤ si ≤ 4).
  * The integers are separated by a space, si is the number of children in the i-th group.
- *
+ * <p>
  * Output
  * Print the single number — the minimum number of taxis necessary to drive all children to Polycarpus.
- *
+ * <p>
  * Examples
  * inputCopy
  * 5
@@ -32,11 +32,11 @@ import java.util.Scanner;
  * 2 3 4 4 2 1 3 1
  * outputCopy
  * 5
- *
- *
+ * <p>
+ * <p>
  * Note
  * In the first test we can sort the children into four cars like this:
- *
+ * <p>
  * the third group (consisting of four children),
  * the fourth group (consisting of three children),
  * the fifth group (consisting of three children),
@@ -49,25 +49,39 @@ public class _13Taxi {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-
         int numberOf = in.nextInt();
-        String word2 = in.next();
-
-        System.out.print(countAmountOfTaxis(numberOf, word2));
+        int number = 0;
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < numberOf; i++) {
+            number = in.nextInt();
+            numbers.add(number);
+        }
+        System.out.print(countAmountOfTaxis(numberOf, numbers));
 
     }
 
-    public static int countAmountOfTaxis(int numberOfGroups, String groups){
-
-        String[] chars = groups.split(" ");
-        Arrays.sort(chars);
-        StringBuilder result = new StringBuilder();
-        int sum = 0;
-
-        for(int i = 0; i <= chars.length-1; i++){
-            sum += Integer.valueOf(chars[i]);
+    public static int countAmountOfTaxis(int numberOfGroups, ArrayList<Integer> numbers) {
+        Integer sum = 0;
+        /*for (Integer nro : numbers) {
+            sum += nro;
         }
+        if(sum%4>0){
+            return ((sum/4)+1);
+        } else return sum / 4;*/
 
-        return sum/4;
+        int counter = 0;
+        int rest = 0;
+        int value = 0;
+        for (Integer nro : numbers) {
+            value= nro + rest;
+            if(value%4==0){
+                counter ++;
+                rest=0;
+            } else {
+                rest= sum%4;
+            }
+
+        }
+        return counter;
     }
 }
